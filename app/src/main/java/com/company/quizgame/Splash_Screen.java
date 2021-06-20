cc481006966bcbcdb252605c0a4892cd0834fefe
Zishuo Xu <xuzishuo1996@gmail.com>
2021-06-20 11:54:16 -0400

    add splash animation. splash on lauch and then invoke main activity

--- app/src/main/java/com/company/quizgame/Splash_Screen.java
+++ app/src/main/java/com/company/quizgame/Splash_Screen.java
@@ -2,13 +2,39 @@ package com.company.quizgame;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class Splash_Screen extends AppCompatActivity {
+
+    ImageView image;
+    TextView title;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        image = findViewById(R.id.imageViewSplash);
+        title = findViewById(R.id.textViewSplash);
+
+        Animation animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.splash_anim);
+        // apply the animation to text view
+        title.startAnimation(animation);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent i = new Intent(Splash_Screen.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, 5000);
     }
 }
